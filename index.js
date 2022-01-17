a48c11f5b454c9ed39eb8bdd3b9809c214a97791
Juan <ssjmatos@gmail.com>
2022-01-17 15:09:57 -0500

    commit tree c7a3e491286866303b941d2d262f4ef3da638f5f

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution here
+function findMatching(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
